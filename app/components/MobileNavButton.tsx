9ce4252b8bef059cdba61ee6d45c0164155f31f5
Nick Kuebler <nickkuebler@DN0a25c94e.SUNet>
2026-02-16 17:03:53 -0800

    Make mobile nav button smaller with larger hitbox

--- app/components/MobileNavButton.tsx
+++ app/components/MobileNavButton.tsx
@@ -45,8 +45,8 @@ export default function MobileNavButton() {
       aria-label="Show navigation"
     >
       <svg
-        width="24"
-        height="24"
+        width="12"
+        height="12"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
